76c817ab70032a5c99d9c5565c448257829902d3
Silvio Paganini <paganini.silvio@bcgplatinion.com>
2021-07-02 12:31:05 +0100

    token not found

--- functions/token.ts
+++ functions/token.ts
@@ -4,6 +4,14 @@ import { tokenProps } from '../db'
 
 const token: APIGatewayProxyHandler = async (event, _context) => {
   try {
+    if (
+      event.pathParameters?.id &&
+      event.pathParameters?.id.length > 0 &&
+      !JSON.stringify(tokenProps[event.pathParameters.id])
+    ) {
+      throw new Error('Token not found')
+    }
+
     return {
       statusCode: 200,
       body:
